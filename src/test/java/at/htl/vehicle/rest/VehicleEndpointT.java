79a861f09101301dc0bcd62e94fcf601c4ecbb20
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-10-13 11:27:18 +0200

    implemented first part of method crud()

--- src/test/java/at/htl/vehicle/rest/VehicleEndpointT.java
+++ src/test/java/at/htl/vehicle/rest/VehicleEndpointT.java
@@ -3,14 +3,19 @@ package at.htl.vehicle.rest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.core.IsNot.not;
 
 public class VehicleEndpointT {
     private Client client;
@@ -32,6 +37,14 @@ public class VehicleEndpointT {
 
     @Test
     public void crud() {
+        Response response = this.target.request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(), is(200));
+        JsonArray payload = response.readEntity(JsonArray.class);
+        System.out.println("payload = " + payload);
+        assertThat(payload, not(empty()));
 
+        JsonObject vehicle = payload.getJsonObject(0);
+        assertThat(vehicle.getString("brand"),equalTo("Opel 44"));
+        assertThat(vehicle.getString("type"),startsWith("Commodore"));
     }
 }
